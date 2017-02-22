a7e663d8350bc22345222442594d9bd6632ac770
Rich Baukovic <rich.baukovic@brickfish.com>
2017-02-21 20:27:39 -0600

    initial revision of backbone collections mini project

--- js/main.js
+++ js/main.js
@@ -1,32 +1,11 @@
-		
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
 
-var Vehicle = Backbone.Model.extend({
-
-	idAttribute: "registrationNumber",
-
-	urlRoot: "/api/vehicles",
-
-	validate: function(attrs){
-		if (!attrs.registrationNumber)
-			return "Vehicle is not valid.";
-	},
-
-	start: function(){
-		console.log("Vehicle started.");
-	}
-});
-
-var Vehicles = Backbone.Collection.extend({
-	Model: Vehicle
-});
-
-var Car = Vehicle.extend({
-	start: function(){
-		console.log("Car with registration number " + this.get("registrationNumber") + " started.");
-	}
+var Vehicles = Backbone.Collection.extend({ 
+	model: Car, 
+ 
+	url: “/api/vehicles 
 });
 
 var vehicles = new Vehicles([
@@ -36,11 +15,5 @@ var vehicles = new Vehicles([
 ]);
 
 var blueCars = vehicles.where({ color: "Blue" });
-console.log("Blue cars", blueCars);
 
-var carXLI887 = vehicles.findWhere({ registrationNumber: "XLI887" });
-console.log("Car with registration number XLI887", carXLI887);
-
-vehicles.remove(carXLI887);
-
-console.log("Vehicles as JSON", vehicles.toJSON());
+console.log(blueCars);
